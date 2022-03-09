66337271fc9798f0f4c7bcac0de9bda023f06d6f
chawin <pathocha000@gmail.com>
2022-03-08 21:04:40 -0800

    Solved #7

--- problems/07-all-the-args.js
+++ problems/07-all-the-args.js
@@ -1,6 +1,23 @@
+// Write a function named allTheArgs(func, ...args) that will accept a
+// function func and any number of arguments after that concatenated 
+// to an array of args.The allTheArgs function should return the passed
+// -in function func modified to always be invoked with the passed -in
+//  arguments args.
 function allTheArgs(func, ...args) {
   // Your code here
+  return func.bind(this, ...args);
 }
+
+const bow = (...names) => {
+  let nameArr = Array.from(names);
+  return "You bowed to " + names.join(" and ");
+};
+
+console.log(bow("Sandy")) // prints "You bowed to Sandy"
+
+let bowSandy = allTheArgs(bow, "Sandy");
+console.log(bowSandy()); // prints "You bowed to Sandy"
+console.log(bowSandy("Joe", "Nico")); // prints "You bowed to Sandy and Joe and Nico"
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
