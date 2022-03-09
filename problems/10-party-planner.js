94db1306d25761680a2b35ba31fda59a03f2aff0
Muhammad <chelseafc56057@gmail.com>
2022-03-09 10:47:28 +0500

    10th problem

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,5 +1,33 @@
 // Your code here
+class PartyPlanner{
+	constructor(guestList){
+		this.guestList = !guestList ? [] : guestList;
+	}
+	addToGuestList(name){
+		this.guestList.push(name)
+	}
+	throwParty(partyStr = "Welcome to the party "){
+		if(this.guestList.length === 0){
+			return  "gotta add people to the guest list";
+		}
+		this.guestList.map((currentGuest, guestIndex) => {
+			partyStr += currentGuest;
+			if(guestIndex !== this.guestList.length - 1)
+				partyStr += " and "
 
+		});
+		return partyStr;
+	}
+}
+const party = new PartyPlanner();
+
+console.log(party.throwParty()); // prints "Gotta add people to the guest list"
+
+party.addToGuestList("James");
+console.log(party.throwParty()); // prints "Welcome to the party James"
+
+party.addToGuestList("Alvin");
+console.log(party.throwParty()); // prints "Welcome to the party James and Alvin"
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
